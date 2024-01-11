af77fd860f42a9deabb0b4bd3d1fdea62ed6b52a
odhyp <pradhana.odhy@gmail.com>
2024-01-11 21:44:45 +0700

    Add is_target_file and is_downloaded method

--- src/managers/file_manager.py
+++ src/managers/file_manager.py
@@ -1,3 +1,5 @@
+from src.common.utils import get_download_path
+
 
 class FileManager:
     def is_xls(self, file_name: str) -> bool:
@@ -15,3 +17,30 @@ class FileManager:
         - file_name (str): The name of the file.
         """
         return file_name.endswith('.xlsx')
+
+    def is_target_file(self, file_name: str) -> bool:
+        """Check if the file is 'CRViewer1.xls'.
+
+        Args:
+        - file_name (str): The name of the file.
+        """
+        target_name = 'CRViewer1.xls'
+        if file_name.find(target_name) == 0:
+            return True
+        else:
+            return False
+
+    def is_downloaded(self) -> bool:
+        """Check if 'CRViewer1.xls' is downloaded.
+
+        Args:
+        - type (str): The type of the file.
+        """
+        for file in get_download_path().iterdir():
+            is_target_file = self.is_target_file(file.name)
+            is_xls = self.is_xls(file.name)
+            if is_target_file and is_xls:
+                return True
+            else:
+                continue
+        return False
